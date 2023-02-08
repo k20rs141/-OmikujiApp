5241cf59336d86ae677b15e3a6bf25659e1e72ed
Yamada Kouki <90810018+k20rs141@users.noreply.github.com>
2023-02-08 23:21:00 +0900

    Add: Setting Bundleの追加

---
+++ App/JRKKL/SupportingFiles/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KsLife
+//
+//  Created by Fuzuki TODA on 12/10/04.
+//  Copyright (c) 2012年 Fuzuki TODA. All rights reserved.
+//
+
+//#import <UIKit/UIKit.h>
+//
+//#import "AppDelegate.h"
+//
+//int main(int argc, char *argv[])
+//{
+//    @autoreleasepool {
+//        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+//    }
+//}
