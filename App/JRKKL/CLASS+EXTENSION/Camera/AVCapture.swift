605fa5ed1f3a6d6fb153ee1237e9e8467bff2ef0
Yamada Kouki <90810018+k20rs141@users.noreply.github.com>
2023-02-09 23:19:21 +0900

    Create: AVCaptureVideoOrientationのExtensionの追加

---
+++ App/JRKKL/CLASS+EXTENSION/Camera/AVCapture.swift
@@ -0,0 +1,26 @@
+import UIKit
+import AVFoundation
+
+// Videoの向き
+extension AVCaptureVideoOrientation {
+    // デバイスの向き
+    init?(deviceOrientation: UIDeviceOrientation) {
+        switch deviceOrientation {
+        case .portrait: self = .portrait
+        case .portraitUpsideDown: self = .portraitUpsideDown
+        case .landscapeLeft: self = .landscapeRight
+        case .landscapeRight: self = .landscapeLeft
+        default: return nil
+        }
+    }
+    // インターフェースの向き
+    init?(interfaceOrientation: UIInterfaceOrientation) {
+        switch interfaceOrientation {
+        case .portrait: self = .portrait
+        case .portraitUpsideDown: self = .portraitUpsideDown
+        case .landscapeLeft: self = .landscapeLeft
+        case .landscapeRight: self = .landscapeRight
+        default: return nil
+        }
+    }
+}
