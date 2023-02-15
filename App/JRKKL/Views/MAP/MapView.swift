48719eb33527b8075396c26e76a98b6b2d6b1915
Yamada Kouki <90810018+k20rs141@users.noreply.github.com>
2023-02-15 16:50:39 +0900

    Create MapView.swift
    Mapを生成するためのファイル

---
+++ App/JRKKL/Views/MAP/MapView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    @ObservedObject var locationManager = LocationManager.shared
+    @Binding var checkInAlert: Bool
+    @Binding var checkInNumber: Int
+    
+    public typealias UIViewType = MKMapView
+    var mapView = MKMapView()
+    
+    public func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "featureAnnotation")
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: NSStringFromClass(KashiiLineAnnotation.self))
+        mapView.selectableMapFeatures = [.pointsOfInterest]
+        mapView.delegate = context.coordinator
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        for i in 0 ..< locationManager.customPin.count {
+            let annotation = CoustomMKPointAnnotation()
+            annotation.coordinate = locationManager.customPin[i].coordinate
+            annotation.title = locationManager.customPin[i].title
+            let overlay = MKCircle(center: locationManager.customPin[i].coordinate, radius: 3)
+            mapView.addAnnotation(annotation)
+            mapView.addOverlay(overlay)
+        }
+        
+        if let userLocation = locationManager.userLocation {
+            let mapRegion = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 100, longitudinalMeters: 100)
+            mapView.setRegion(mapRegion, animated: true)
+        }
+        return mapView
+    }
+    
+    public func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        if let userLocation = locationManager.userLocation {
+            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            let region = MKCoordinateRegion(center: userLocation.coordinate, span: span)
+            uiView.setRegion(region, animated: false)
+        }
+    }
+    
+    public func makeCoordinator() -> MapViewCoordinator {
+        MapViewCoordinator(parent: self)
+    }
+}
