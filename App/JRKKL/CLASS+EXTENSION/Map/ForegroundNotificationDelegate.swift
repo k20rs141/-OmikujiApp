759f548bc119b09d1acd0a8f8a32e0a701ce64e7
Yamada Kouki <90810018+k20rs141@users.noreply.github.com>
2023-02-24 23:48:26 +0900

    Create ForegroundNotificationDelegate.swift
    フォアグラウンド時にも通知を有効化するためのDelegate

---
+++ App/JRKKL/CLASS+EXTENSION/Map/ForegroundNotificationDelegate.swift
@@ -0,0 +1,8 @@
+import UserNotifications
+
+// フォアグラウンド通知用
+class ForegroundNotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .list, .badge, .sound])
+    }
+}
