3609876c653786b4836670ac209c88baef7afb3d
Yamada Kouki <90810018+k20rs141@users.noreply.github.com>
2023-03-01 23:56:31 +0900

    Improve: 即時通知に変更

--- App/JRKKL/CLASS+EXTENSION/Map/NotificationModel.swift
+++ App/JRKKL/CLASS+EXTENSION/Map/NotificationModel.swift
@@ -24,17 +24,13 @@ class NotificationModel: ObservableObject {
     }
 
     func makeNotification() {
-        let notificationDate = Date().addingTimeInterval(1) // 1秒後
-        let dateComp = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: notificationDate)
-        // 日時でトリガー指定
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComp, repeats: false)
         // 通知内容
         let content = UNMutableNotificationContent()
         content.title = "JR九州香椎線"
         content.body = "まもなく到着します"
         content.sound = UNNotificationSound.default
         // リクエスト作成
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
         // 通知をセット
         UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
         // 通知のテキストを音声で読み上げ
