b7f0e194e63b1aad0c92de2a4deef4ee1616ec59
Yamada Kouki <90810018+k20rs141@users.noreply.github.com>
2023-03-04 23:59:54 +0900

    Change: UIの変更

--- App/JRKKL/Views/MAP/CheckInView.swift
+++ App/JRKKL/Views/MAP/CheckInView.swift
@@ -9,81 +9,58 @@ struct CheckInView: View {
     
     var body: some View {
         VStack(spacing: 0) {
-            if (locationManager.customPin.count > 8) {
-                ZStack {
-                    Text(locationManager.customPin[checkInNumber].title)
-                        .foregroundColor(.white)
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .frame(maxWidth: .infinity, maxHeight: screen.height * 0.06)
-                        .background(Color("JRKyusyuColor"))
-                    HStack {
-                        Button {
-                            self.checkInView = false
-                        } label: {
-                            Image(systemName: "xmark")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 20, height: 20)
-                                .foregroundColor(Color("JRKyusyuColor"))
-                                .background {
-                                    Circle()
-                                        .strokeBorder(Color("JRKyusyuColor"),lineWidth: 2)
-                                        .background(Circle().foregroundColor(Color("PopUpViewColor")))
-                                        .frame(width: 45, height: 45)
-                                }
-                        }
-                        .padding(.leading)
-                        Spacer()
-                    }
-//                    .offset(x: screen.width * -0.37, y: screen.height * -0.015)
-                }
+            HStack(spacing: 0) {
                 VStack {
-                    Spacer()
-                    Image(systemName: "mappin.circle.fill")
+                    Image("\(locationManager.customPin[checkInNumber].image)_station")
                         .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 60, height: 60)
+                        .scaledToFill()
+                        .frame(maxWidth: 75, maxHeight: 75)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                }
+                .frame(maxWidth: screen.width * 0.25, maxHeight: .infinity)
+                VStack(alignment: .leading, spacing: 10) {
+                    Text(locationManager.customPin[checkInNumber].title)
+                        .font(.title2)
+                    Text("福岡県福岡市東区")
+                        .font(.caption)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                VStack(alignment: .leading) {
+                    Text("自動アラート")
                         .foregroundColor(Color("JRKyusyuColor"))
-                    Spacer()
-                    if locationManager.customPin[checkInNumber].checked {
-                        Text("チェックイン済みです")
-                            .foregroundColor(.gray.opacity(0.5))
-                            .padding(.bottom)
-                    } else {
-                        Button {
-                            locationManager.checkLocation(checkInNumber: checkInNumber)
-                        } label: {
-                            Text("チェックインする")
-                                .foregroundColor(.white)
-                                .font(.callout)
-                                .fontWeight(.bold)
-                                .frame(width: screen.width * 0.45, height: screen.height * 0.04)
-                                .background(.red)
-                                .cornerRadius(25)
-                                .padding(.bottom)
-                        }
-                    }
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity, maxHeight: screen.height * 0.035)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                            .stroke(lineWidth: 1)
+                            .foregroundColor(Color("JRKyusyuColor"))
+                        )
+                        .padding(.trailing)
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                Divider()
-                VStack {
-                    Button {
-                        locationManager.moniteringStart(moniteringNumber: checkInNumber)
-                    } label: {
-                        Text("開始")
-                            .foregroundColor(.white)
-                            .font(.callout)
-                            .fontWeight(.bold)
-                            .frame(width: screen.width * 0.25, height: screen.height * 0.04)
-                            .background(.red)
-                            .cornerRadius(25)
-                            .padding(.bottom)
-                    }
-                }
-                .frame(maxWidth: .infinity, maxHeight: screen.height * 0.13)
+                .frame(maxWidth: screen.width * 0.3, maxHeight: .infinity)
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            HStack {
+                Button {
+                    locationManager.checkLocation(checkInNumber: checkInNumber)
+                } label: {
+                    Text("チェックイン")
+                        .foregroundColor(.white)
+                        .font(.callout)
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity, maxHeight: screen.height * 0.045)
+                        .background(Color("JRKyusyuColor"))
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: screen.height * 0.055)
         }
+        .frame(maxWidth: .infinity, maxHeight: screen.height * 0.17)
+        .background(Color("PopUpViewColor"))
+        .cornerRadius(15)
+        .padding(.horizontal)
         .alert("チェックイン範囲外です。範囲内に移動してください!", isPresented: $locationManager.checkInAlert) {
             Button("OK") {
                 
@@ -92,9 +69,6 @@ struct CheckInView: View {
         .sheet(isPresented: $locationManager.isAnimation) {
             AnimationView(locationManager: locationManager, checkInNumber: $checkInNumber)
         }
-        .frame(width: screen.width * 0.8, height: screen.height * 0.4, alignment: .center)
-        .background(Color("PopUpViewColor"))
-        .cornerRadius(12)
     }
 }
 
