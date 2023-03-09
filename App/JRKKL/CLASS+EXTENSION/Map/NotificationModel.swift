f57e212e2f8d68fdd064e01f00b409da90d5ba4b
Yamada Kouki <90810018+k20rs141@users.noreply.github.com>
2023-03-09 23:32:08 +0900

    Improve: 音声読み上げの切り替えと読み上げ速度の変更

--- App/JRKKL/CLASS+EXTENSION/Map/NotificationModel.swift
+++ App/JRKKL/CLASS+EXTENSION/Map/NotificationModel.swift
@@ -5,6 +5,8 @@ import AVFoundation
 class NotificationModel: ObservableObject {
     let synthesizer = AVSpeechSynthesizer()
     var notificationDelegate = ForegroundNotificationDelegate()
+    let title = "JR九州香椎線"
+    let body = "まもなく到着します"
 
     init() {
         // フォアグラウンド通知用
@@ -13,10 +15,14 @@ class NotificationModel: ObservableObject {
         UNUserNotificationCenter.current().removeAllDeliveredNotifications()
     }
 
-    func setNotification() {
+    func setNotification(notify: Bool) {
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
             if granted {
                 self.makeNotification()
+                // trueの時、音声読み上げ
+                if notify {
+                    self.speechSynthesizer()
+                }
             } else {
 
             }
@@ -26,24 +32,23 @@ class NotificationModel: ObservableObject {
     func makeNotification() {
         // 通知内容
         let content = UNMutableNotificationContent()
-        content.title = "JR九州香椎線"
-        content.body = "まもなく到着します"
+        content.title = title
+        content.body = body
         content.sound = UNNotificationSound.default
         // リクエスト作成
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
         // 通知をセット
         UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
-        // 通知のテキストを音声で読み上げ
-        speechSynthesizer(comment: request.content.body)
     }
 
     func removeNotification() {
         UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [UUID().uuidString])
     }
     
-    func speechSynthesizer(comment: String) {
-        let utterance = AVSpeechUtterance(string: comment)
+    func speechSynthesizer() {
+        let utterance = AVSpeechUtterance(string: body)
         utterance.voice = makeVoice("com.apple.voice.enhanced.ja-JP.Kyoko")
+        utterance.rate = 0.5
         synthesizer.speak(utterance)
     }
     // 日本語ボイスの生成
