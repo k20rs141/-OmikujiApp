20d4c47db4c50d863bac3c44b0641df2f2bcf897
Yamada Kouki <90810018+k20rs141@users.noreply.github.com>
2023-03-11 23:13:19 +0900

    Improve: リファクタリング

--- App/JRKKL/CLASS+EXTENSION/Map/ForegroundNotificationDelegate.swift
+++ App/JRKKL/CLASS+EXTENSION/Map/ForegroundNotificationDelegate.swift
@@ -5,7 +5,7 @@ class ForegroundNotificationDelegate: NSObject, UNUserNotificationCenterDelegate
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.banner, .list, .badge, .sound])
     }
-    
+    // 通知をタップしてアプリを起動した時
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         UNUserNotificationCenter.current().removeAllDeliveredNotifications()
         completionHandler()
