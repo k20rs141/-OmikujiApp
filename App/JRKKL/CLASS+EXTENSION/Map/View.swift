a30f6e56d3d2cd2b585fb757defca5dd42d65911
Yamada Kouki <90810018+k20rs141@users.noreply.github.com>
2023-03-13 16:45:19 +0900

    Improve: モディファイアの共通化

---
+++ App/JRKKL/CLASS+EXTENSION/Map/View.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+extension View {
+    func cardStyle() -> some View {
+        self
+            .padding()
+            .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.07)
+            .background(Color(red: 0.97, green: 0.97, blue: 0.97))
+            .cornerRadius(13)
+    }
+    
+    func textStyle() -> some View {
+        self
+            .foregroundColor(.gray)
+            .font(.title3)
+            .frame(width: UIScreen.main.bounds.width * 0.33, height: UIScreen.main.bounds.height * 0.07, alignment: .trailing)
+    }
+    
+    func buttonStyle(color: Color, cornerRadius: CGFloat) -> some View {
+        self
+            .foregroundColor(color)
+            .padding()
+            .background(.ultraThickMaterial)
+            .cornerRadius(cornerRadius)
+    }
+}
