16c170cdbf9cedd6ae48c9fff19163f1056e8722
Yamada Kouki <90810018+k20rs141@users.noreply.github.com>
2023-03-18 23:02:04 +0900

    Change: NotificationModelのExtensionに変更
    通知タップ時に音声を停止する機能も追加

--- App/JRKKL/CLASS+EXTENSION/Map/NotificationModel+Extension.swift
+++ App/JRKKL/CLASS+EXTENSION/Map/NotificationModel+Extension.swift
@@ -1,13 +1,18 @@
 import UserNotifications
 
 // フォアグラウンド通知用
-class ForegroundNotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
+extension NotificationModel: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.banner, .list, .badge, .sound])
     }
     // 通知をタップしてアプリを起動した時
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+        stopSpeechSynthesizer()
+        removeNotificationRequests = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.removeNotificationRequests = false
+        }
         completionHandler()
     }
 }
