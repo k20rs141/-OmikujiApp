8cfa56d8c92afa551231940004f3b41bd9777ab0
Yamada Kouki <90810018+k20rs141@users.noreply.github.com>
2023-03-21 00:11:39 +0900

    Add: SettingViewのヘルプ画面の追加

---
+++ App/JRKKL/Views/MAP/ExplanationView.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+struct ExplanationView: View {
+    @ObservedObject var locationManager: LocationManager
+    @Binding var explanationView: Bool
+    @Binding var title: Int
+
+    let screen = UIScreen.main.bounds
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Text(locationManager.information[title].title)
+                .foregroundColor(.gray)
+                .font(.title)
+                .fontWeight(.bold)
+            ScrollView {
+                Text(locationManager.information[title].detail)
+                    .foregroundColor(.gray)
+                    .padding()
+            }
+            .frame(maxWidth: .infinity, maxHeight: screen.height * 0.14)
+            .padding(.vertical)
+            HStack {
+                Button {
+                    explanationView = false
+                } label: {
+                    Text("確認")
+                        .foregroundColor(.black)
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+            }
+        }
+        .frame(maxWidth: screen.width * 0.85, maxHeight: screen.height * 0.3)
+        .background(.white)
+        .cornerRadius(15)
+        .padding(.horizontal)
+    }
+}
+
+struct ExplanationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExplanationView(locationManager: LocationManager(), explanationView: .constant(true), title: .constant(0))
+    }
+}
